d4380ba07bf0f0c5b741da63e152eaa1d75e875d
Aleysyah <naleysyah@gmail.com>
2025-06-19 05:13:10 +0800

    Update home.spec.ts

--- frontend/src/app/components/home/home.spec.ts
+++ frontend/src/app/components/home/home.spec.ts
@@ -1,18 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomeComponent } from './home';
+import { HttpClient } from '@angular/common/http';
+import { CartService } from '../../services/cart.service';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer/footer';
 
-import { Home } from './home';
-
-describe('Home', () => {
-  let component: Home;
-  let fixture: ComponentFixture<Home>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home]
-    })
-    .compileComponents();
+      imports: [RouterModule, FooterComponent],
+      declarations: [HomeComponent],
+      providers: [
+        { provide: HttpClient, useValue: { get: jasmine.createSpy('get').and.returnValue({ subscribe: () => {} }) } },
+        { provide: CartService, useValue: { addToCart: jasmine.createSpy('addToCart') } }
+      ]
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(Home);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
